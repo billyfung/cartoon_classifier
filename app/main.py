e4a3e1e67cfeb419977ea748a1ceb2038e383f69
Billy <billy@billyfung.com>
2020-04-10 14:14:57 +1200

    upload and getting url works async

--- app/main.py
+++ app/main.py
@@ -6,42 +6,43 @@ import torch
 from pathlib import Path
 from io import BytesIO
 import aiohttp, asyncio
+import logging
 
 MODEL_URL = 'https://drive.google.com/u/0/uc?id=1-Cw70xsI7RBKIFWYvGkElUbW3RZUtacw&export=download'
-MODEL_FILENAME = 'model'
+MODEL_FILENAME = 'export'
 
 app = FastAPI()
 
 path = Path(__file__).parent
 
+@app.on_event("startup")
+async def setup_learner():
+    await download_file(MODEL_URL, path/f'{MODEL_FILENAME}.pkl')
+    global learner
+    learner = load_learner(path)
+
 async def download_file(url, dest):
-    if dest.exists(): return
+    if dest.exists(): 
+        global learner
+        learner = load_learner(path)
+        return
     async with aiohttp.ClientSession() as session:
+        logging.info('getting model file')
         async with session.get(url) as response:
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
-
-async def setup_learner():
-    await download_file(model_file_url, path/f'{model_file_name}.pth')
-    learner = load_learner(path)
-    return learn
 
 async def get_bytes(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.read()
 
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future(setup_learner())]
-learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
-loop.close()
-
 def predict_image_from_bytes(bytes):
     img = open_image(BytesIO(bytes))
-    _,_,losses = learn.predict(img)
+    _,_,losses = learner.predict(img)
     return {
         "predictions": sorted(
-            zip(learn.data.classes, map(float, losses)),
+            zip(learner.data.classes, map(float, losses)),
             key=lambda p: p[1],
             reverse=True
         )
@@ -71,9 +72,10 @@ async def show_form():
 
 @app.get("/classify-url")
 async def classify_url(url: str):
+    logging.info(f'getting url')
     bytes = await get_bytes(url)
     return predict_image_from_bytes(bytes)
 
 @app.post("/upload")
 async def upload_file(file: bytes = File(...)):
-    return predict_image_from_bytes(file)git
+    return predict_image_from_bytes(file)
